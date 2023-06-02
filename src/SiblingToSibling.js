4a1aeb3e163e7c7b096f86ff33c3e245a3cf5d27
eduardo1905 <128932100+eduardo1905@users.noreply.github.com>
2023-06-02 17:37:08 -0400

    2 files modified

--- src/SiblingToSibling.js
+++ src/SiblingToSibling.js
@@ -9,22 +9,28 @@ const EMPTY_MESSAGE = {
 export const SiblingToSibling = () => {
   const [message, setMessage] = useState(EMPTY_MESSAGE);
 
+  const onMessage = (message) => {
+    setMessage(message);
+  };
+
   return (
     <>
       <h2>SiblingToSibling</h2>
 
-      <Foo message={message} />
+      <Foo message={message} onMessage={onMessage} />
 
-      <Bar message={message} />
+      <Bar message={message} onMessage={onMessage} />
+
+      <Messenger name={'baz'} message={message} onMessage={onMessage} />
     </>
   );
 };
 
 const Foo = (props) => {
   const message = props.message;
-  
+
   const [text, setText] = useState('');
-  
+
   const onChange = (e) => {
     setText(e.target.value);
   };
@@ -39,10 +45,14 @@ const Foo = (props) => {
   return (
     <>
       <h3>Foo</h3>
-      <div>last message from '{message.from}': {message.text}</div>
+      <div>
+        last message from '{message.from}': {message.text}
+      </div>
       <div className="input-group">
         <input className="form-control" onChange={onChange} value={text} />
-        <button className="btn btn-primary">submit</button>
+        <button className="btn btn-primary" onClick={onClick}>
+          submit
+        </button>
       </div>
     </>
   );
@@ -52,18 +62,67 @@ const Foo = (props) => {
 const Bar = (props) => {
   const message = props.message;
 
+  const [text, setText] = useState('');
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onClick = () => {
+    if (props.onMessage) {
+      props.onMessage({ from: 'bar', text });
+    }
+  };
+
   return (
     <>
       <h3>Bar</h3>
-      <div>last message from '{message.from}': {message.text}</div>
+      <div>
+        last message from '{message.from}': {message.text}
+      </div>
       <div className="input-group">
-        <input className="form-control" />
-        <button className="btn btn-primary">submit</button>
+        <input className="form-control" onChange={onChange} value={text} />
+        <button className="btn btn-primary" onClick={onClick}>
+          submit
+        </button>
       </div>
     </>
   );
 };
 
 // BONUS: Foo and Bar have a lot of duplicate functionality. Create a component
-// that abstracts this away.
-const Messenger = () => {};
+// that abstracts this away. Add it as a Baz sibling.
+const Messenger = (props) => {
+  const name = props.name;
+  const message = props.message;
+  const onMessage = props.onMessage;
+
+  const [text, setText] = useState('');
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onClick = () => {
+    if (onMessage) {
+      onMessage({ from: name, text });
+    }
+  };
+
+  return (
+    <>
+      <h3>{capitalize(name)}</h3>
+      <div>
+        last message from '{message.from}': {message.text}
+      </div>
+      <div className="input-group">
+        <input className="form-control" onChange={onChange} value={text} />
+        <button className="btn btn-primary" onClick={onClick}>
+          submit
+        </button>
+      </div>
+    </>
+  );
+};
+
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
